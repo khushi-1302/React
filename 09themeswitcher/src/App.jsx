9e4ae3caf8d238821f823f7ffa408c3a30f868c6
khushi-1302 <khushijalan1302@gmail.com>
2024-05-23 12:01:20 +0530

    sixth commit

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+
+function App() {
+   
+
+  return (
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    
+      <div className="flex flex-wrap min-h-screen items-center">
+  <div className="w-full">
+   <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+      {/* themeBtn */}
+                        
+    </div>
+
+    <div className="w-full max-w-sm mx-auto">
+      {/* Card */}
+                       
+    </div>
+  </div>
+      </div>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
